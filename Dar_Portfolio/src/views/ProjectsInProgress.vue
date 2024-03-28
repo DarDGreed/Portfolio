eb323893c3174ebf75d2b691b9a3d8772940da2a
D4rdose <141896824+D4rdose@users.noreply.github.com>
2024-03-28 19:28:09 +0800

    logo to be added
    testimonials and in progress projects added

--- Dar_Portfolio/src/views/ProjectsInProgress.vue
+++ Dar_Portfolio/src/views/ProjectsInProgress.vue
@@ -2,7 +2,39 @@
 
 </script>
 <template>
-    <div>
-        <h1>Hello world</h1>
+    <div class="container">
+        <div class="inProgress">
+            <img class="image" src="@/assets/images/realtimechat.jpg" alt="realtime chat image">
+            <a class="project_name" href="https://github.com/DarDGreed/TUF">Realtime Chat App</a>
+        </div>
     </div>
 </template>
+
+<style scoped>
+.container {
+    margin: 30px 60px 30px 60px;
+    display: flex;
+}
+.inProgress {
+    width: 275px;
+    height: 275px;
+    margin-right: 75px;
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+}
+.image {
+    width: 275px;
+    height: 225px;
+    border-radius: 60px;
+}
+.project_name {
+    color: rgb(3, 139, 230);
+    font-weight: bolder;
+    font-size: 30px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+</style>
--- Dar_Portfolio/src/views/Testimonials.vue
+++ Dar_Portfolio/src/views/Testimonials.vue
@@ -1,8 +1,44 @@
 <script setup>
 
 </script>
+
 <template>
-    <div>
-        <h1>Hello world</h1>
+    <div class="container">
+        <div class="testimonial_container">
+            <video id="testimonials" class="testimonials" controls>
+                <source src="@/assets/videos/sample.mp4">
+            </video>
+            <span class="title">Notes App Testimony</span>
+        </div>
+        <div class="testimonial_container">
+            <video id="testimonials" class="testimonials" controls>
+                <source src="@/assets/videos/sample.mp4">
+            </video>
+            <span class="title">Todo Project Testimony</span>
+        </div>
     </div>
 </template>
+
+<style scoped>
+.container {
+    margin: 30px 60px 30px 60px;
+    display: flex;
+}
+.testimonial_container {
+    width: 375px;
+    height: 325px;
+    margin-right: 75px;
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+}
+.title {
+    color: rgb(3, 139, 230);
+    font-weight: bolder;
+    font-size: 30px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-decoration: none;
+}
+</style>
